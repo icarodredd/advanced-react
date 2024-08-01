79d3b6ab78cbd3b20bece8bc2e06f29b6fcec45c
icarodredd <icarog854@gmail.com>
2024-08-01 10:05:40 -0300

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+
+const globalState = {
+  title: "Title",
+  count: 0,
+};
 
 //useContext
 const GlobalContext = createContext();
@@ -8,17 +13,25 @@ const UseContext = () => {
   const context = useContext(GlobalContext);
   return (
     <div>
-      <h1>{context.text}</h1>
+      <h1>{context.title}</h1>
     </div>
   );
 };
 
+//useReducer
+const reducer = (state, action) => {
+  if (action.type === "Change")
+    return { ...state, title: "The text has changed!" };
+  return { ...state };
+};
+
 function App() {
-  //useContext
+  const [state, dispatch] = useReducer(reducer, globalState);
 
   return (
-    <GlobalContext.Provider value={{ text: "Example of useContext" }}>
+    <GlobalContext.Provider value={state}>
       <UseContext />
+      <button onClick={() => dispatch({ type: "Change" })}>Click</button>
     </GlobalContext.Provider>
   );
 }
