0a5c7eac91184d90f5a3b41baaccba6d02c60aae
icarodredd <icarog854@gmail.com>
2024-08-02 14:24:54 -0300

    context + hooks part 2

---
+++ src/contexts/CounterContext/types.js
@@ -0,0 +1,7 @@
+export const INCREASE = "CounterContext/INCREASE";
+export const DECREASE = "CounterContext/DECREASE";
+export const RESET = "CounterContext/RESET";
+export const SET_COUNTER = "CounterContext/SET_COUNTER";
+export const ASYNC_INCREASE_START = "CounterContext/ASYNC_INCREASE_START";
+export const ASYNC_INCREASE_END = "CounterContext/ASYNC_INCREASE_END";
+export const ASYNC_INCREASE_ERROR = "CounterContext/ASYNC_INCREASE_ERROR";
