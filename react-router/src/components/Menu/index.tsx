5ec56dfcadcb2a366d82755845da1583f115d0b5
icarodredd <icarog854@gmail.com>
2024-08-05 14:19:01 -0300

    using link

---
+++ react-router/src/components/Menu/index.tsx
@@ -0,0 +1,5 @@
+import { Link } from "react-router-dom";
+
+export default function Menu() {
+  return <Link to={"/posts"}>Posts</Link>;
+}
--- react-router/src/main.tsx
+++ react-router/src/main.tsx
@@ -2,11 +2,15 @@ import ReactDOM from "react-dom/client";
 import Home from "./components/Home";
 import "./styles/global.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Posts from "./components/Posts";
+import Menu from "./components/Menu";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
+    <Menu />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/posts" element={<Posts />} />
     </Routes>
   </BrowserRouter>
 );
